c51d06ba4cc2c70ad54f5c48b13c973776ccea77
Toby Portelli <tobyportelli@gmail.com>
2021-11-09 11:29:44 -0500

    Display a menu so user can choose arithmetic or exit

--- calc.sh
+++ calc.sh
@@ -10,5 +10,21 @@
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
 
+sum=0
+calc="y"
+
 echo "Hello! Welcome to my calculator. You can do the 4 basic arithnmetic operations of adding, subtracting, 
 multiplying and dividing. Please type out the operation you would like to use:"
+
+while [ $calc = "y" ]
+do
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+echo "5. Exit"
+read response
+
+
+
+
