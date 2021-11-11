3d8241ca40d73f824a27ddedca4246830113b994
Toby Portelli <tobyportelli@gmail.com>
2021-11-10 20:54:49 -0500

    set a while loop to keep user in menu unless exited, simple arithmetic to take 2 numbers and get an answer

--- calc.sh
+++ calc.sh
@@ -10,24 +10,62 @@
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
 
-sum=0
-calc="y"
+ch=0
 
 echo "Hello! Welcome to my calculator. You can do the 4 basic arithnmetic operations of adding, subtracting, 
-multiplying and dividing. Please type out the operation you would like to use:"
+multiplying and dividing."
 
-while [ $calc = "y" ]
+echo
+
+while [ true ] 
 do
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Division"
-echo "5. Exit"
-read response
 
-echo "Please enter first number:"
-read n1
-echo "Please enter second number:"
-read n2
+  echo "Please enter the number for the operation you want to use:"
+  echo "1. Addition"
+  echo "2. Subtraction"
+  echo "3. Multiplication"
+  echo "4. Division"
+  echo "5. Exit"
+  read ch
+
+if  [ $ch -eq "1" ] || [ $ch -eq "2" ] || [ $ch -eq "3" ] || [ $ch -eq "4" ] || [ $ch -eq "5" ] 
+then 
+
+  if [ $ch -eq "5" ]
+  then
+    break
+  fi
+
+  echo "Please enter your first number:"
+  read n1
+  echo "Please enter your second number:"
+  read n2
 
 
+  let res="$n1 + $n2"
+    case $ch in
+      1) let res="$n1 + $n2" ;;
+      2) let res="$n1 - $n2" ;;
+      3) let res="$n1 * $n2" ;;
+      4) let res="$n1 / $n2" ;;
+    esac
+
+
+
+
+  # case $ch in
+  #   1) res=`echo ("$n1" + "$n2") ` ;;
+  #   2) res=`echo ($n1 - $n2) ` ;;
+  #   3) res=`echo ($n1 \* $n2) ` ;;
+  #   4) res=`echo ($n1 / $n2) ` ;;
+  # esac
+
+
+  echo "The result is $res"
+
+else
+  echo
+  echo "You did not enter a known option, please enter 1-4 or 5 to exit:"
+  echo
+fi
+done
